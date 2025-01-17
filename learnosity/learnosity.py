462e8bd41ed5d19c1d1a9aeb05499fdfc5207759
Chamara Ranatunga <chamara.r@euka.edu.au>
2025-01-17 08:14:11 +0530

    studio interfate add

--- learnosity/learnosity.py
+++ learnosity/learnosity.py
@@ -4,7 +4,6 @@ from xblock.fields import String, Scope
 from learnosity_sdk.request import Init
 from learnosity_sdk.utils import Uuid
 from web_fragments.fragment import Fragment
-from django.contrib.auth.models import User
 
 try:
     from xblock.utils.resources import ResourceLoader  # pylint: disable=ungrouped-imports
